c12b5081d10a78e7d09726eeb485050b29c58602
yiwei ding <yiweiding0819@gmail.com>
2024-02-12 16:03:07 -0500

    ring buffer

--- src/ring_buffer.rs
+++ src/ring_buffer.rs
@@ -1,64 +1,81 @@
 pub struct RingBuffer<T> {
     // TODO: fill this in.
+    buffer: Vec<T>,
+    read_index: usize,
+    write_index: usize,
 }
 
 impl<T: Copy + Default> RingBuffer<T> {
     pub fn new(length: usize) -> Self {
         // Create a new RingBuffer with `length` slots and "default" values.
         // Hint: look into `vec!` and the `Default` trait.
-        todo!()
+        Self {
+            buffer: vec![T::default(); length],
+            read_index: 0,
+            write_index: 0
+        }
     }
 
     pub fn reset(&mut self) {
         // Clear internal buffer and reset indices.
-        todo!()
+        self.buffer.clear();
+        self.read_index = 0;
+        self.write_index = 0;
     }
 
     // `put` and `peek` write/read without advancing the indices.
     pub fn put(&mut self, value: T) {
-        todo!()
+        self.buffer[self.write_index] = value;
     }
 
     pub fn peek(&self) -> T {
-        todo!()
+        self.buffer[self.read_index]
     }
 
     pub fn get(&self, offset: usize) -> T {
-        todo!()
+        self.buffer[(self.read_index + offset) % self.capacity()]
     }
 
     // `push` and `pop` write/read and advance the indices.
     pub fn push(&mut self, value: T) {
-        todo!()
+        self.buffer[self.write_index] = value;
+        self.write_index += 1;
     }
 
     pub fn pop(&mut self) -> T {
-        todo!()
+        let output = self.buffer[self.read_index];
+        self.read_index += 1;
+        output
     }
 
     pub fn get_read_index(&self) -> usize {
-        todo!()
+        self.read_index
     }
 
     pub fn set_read_index(&mut self, index: usize) {
-        todo!()
+        self.read_index = index;
     }
 
-    pub fn get_write_index(&self) -> T {
-        todo!()
+    pub fn get_write_index(&self) -> usize {
+        self.write_index
     }
 
     pub fn set_write_index(&mut self, index: usize) {
-        todo!()
+        self.write_index = index;
     }
 
     pub fn len(&self) -> usize {
         // Return number of values currently in the buffer.
-        todo!()
+        if self.read_index >= self.write_index {
+            self.read_index - self.write_index
+        }
+        else {
+            self.read_index + self.capacity() - self.write_index
+        }
     }
 
     pub fn capacity(&self) -> usize {
         // Return the length of the internal buffer.
-        todo!()
+        self.buffer.len()
     }
 }
